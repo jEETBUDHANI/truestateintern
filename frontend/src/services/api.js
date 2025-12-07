703ccb6f6599d19b954445416fbbf32e3da2c9eb
jEETBUDHANI <sk0471011@gmail.com>
2025-12-07 13:08:00 +0530

    "Point frontend to Render backend"

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://truestateintern.onrender.com',
 });
+
 
 export function fetchTransactions(params){
   return client.get('/api/transactions', { params })
